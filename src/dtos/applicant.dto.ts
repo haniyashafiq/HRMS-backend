dd067117665751e5ae5ca1c59ca4834407eadb75
haniyashafiq <haniyashafiq2005@gmail.com>
2025-11-16 00:45:01 +0500

    added trim in DTO

--- src/dtos/applicant.dto.ts
+++ src/dtos/applicant.dto.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 export const ApplicantSchema = z.object({
-  name: z.string().min(2),
-  cnic: z.string(),
-  city: z.string(),
-  qualification: z.string(),
-  previousOrg: z.string(),
-  contactNumber: z.string(),
-  whatsappNumber: z.string(),
-  email: z.email(),
-  linkedin: z.string().optional(),
-  domainApplied: z.string(),
-  tools: z.array(z.string()),
-  github: z.string().optional()
+  name: z.string().min(2).trim(),
+  cnic: z.string().trim(),
+  city: z.string().trim(),
+  qualification: z.string().trim(),
+  previousOrg: z.string().trim(),
+  contactNumber: z.string().trim(),
+  whatsappNumber: z.string().trim(),
+  email: z.email().trim(),
+  linkedin: z.string().trim().optional(),
+  domainApplied: z.string().trim(),
+  tools: z.array(z.string().trim()),
+  github: z.string().trim().optional()
 });
 
 // For TypeScript
